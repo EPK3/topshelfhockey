1e2283c63730ef2d15c693df987d22aea5814727
epk3 <ekrajewski3@protonmail.com>
2022-04-05 15:38:21 -0400

    inital commit

---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import outdoorRink from "./img/outdoor-rink-tsh.jpg";
+import './Banner.css';
+
+const Banner = () => {
+    return(
+        <div className = "banner">
+            
+        </div>
+    );
+}
+
+export default Banner;
