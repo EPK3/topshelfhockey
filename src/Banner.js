7ce064b49d2c20e5c4f61009446ad84cf4b5415f
epk3 <ekrajewski3@protonmail.com>
2022-04-05 22:34:35 -0400

    updated nav

--- src/Banner.js
+++ src/Banner.js
@@ -4,7 +4,12 @@ import './Banner.css';
 const Banner = () => {
     return(
         <div className = "banner">
-            
+            <div className='sale'>
+                <h4>summer sale</h4>
+                <h3>up to</h3>
+                <h1>80%</h1>
+                <h5>off</h5>
+            </div>
         </div>
     );
 }
